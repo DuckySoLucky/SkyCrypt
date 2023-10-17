c76f63a8c14f9aec94c07f9cea6c9b356346baaf
DuckySoLucky <robertkovac160@gmail.com>
2023-10-17 13:48:04 +0200

    feat: bump FurfSky Reborn Resource Pack

--- src/custom-resources.js
+++ src/custom-resources.js
@@ -19,7 +19,7 @@ import { getFileHash } from "./hashes.js";
 const execFile = util.promisify(child_process.execFile);
 
 const NORMALIZED_SIZE = 128;
-const RESOURCE_CACHING = true;
+const RESOURCE_CACHING = false;
 
 const FOLDER_PATH = getFolderPath();
 const RESOURCE_PACK_FOLDER = path.resolve(getFolderPath(), "..", "public", "resourcepacks");
@@ -234,7 +234,7 @@ async function loadResourcePacks() {
             }
           }
         } catch (e) {
-          //
+          // console.log("Couldn't find", e && e.path ? e.path.split("/resourcepacks/")[1] : e);
         }
       }
 
